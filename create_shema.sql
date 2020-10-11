9282e05bc51e40e1b8987818b29a2e9844b9a88e
Denis Plotnicov <kapu4inko@gmail.com>
2020-10-11 20:32:10 +0300

    Schema script reconfigured

--- create_shema.sql
+++ create_shema.sql
@@ -3,6 +3,11 @@ CREATE TYPE type_movement_fund AS ENUM (
   'output'
 );
 
+CREATE TABLE currency (
+  id smallint PRIMARY KEY,
+  currency_name varchar
+);
+
 CREATE TYPE type_account AS ENUM (
   'debit',
   'credit'
@@ -31,16 +36,27 @@ CREATE TYPE initiator_type AS ENUM (
     'system'
 );
 
-CREATE TABLE instrument (
-        id int PRIMARY KEY,
-        delete_date datetime,
-        lot_size int,
-        trading_start_date datetime,
-        instrument_template_id smallint NOT NULL REFERENCES  exchange_db.instrument_template(id),
-        market_id smallint NOT NULL REFERENCES exchange_db.market(id)
+CREATE TABLE country (
+  id smallint PRIMARY KEY,
+  country_name varchar
 );
 
-CREATE TABLE exchange_db.instrument_template (
+
+CREATE TABLE time_zone (
+  id smallint PRIMARY KEY,
+  utc_time_zone varchar
+);
+
+CREATE TABLE market (
+        id int PRIMARY KEY,
+        name varchar,
+        open_time timestamp,
+        close_time timestamp,
+        delete_date timestamp,
+        currency smallint NOT NULL REFERENCES currency(id)
+);
+
+CREATE TABLE instrument_template (
         id int PRIMARY KEY,
         short_name varchar,
         long_name varchar,
@@ -48,107 +64,98 @@ CREATE TABLE exchange_db.instrument_template (
         coupon_amount float,
         coupon_payment_frequency smallint,
         isin varchar,
-        maturity_date datetime,
+        maturity_date timestamp,
         emission_volume float,
-        emission_date datetime,
-        delete_date datetime,
+        emission_date timestamp,
+        delete_date timestamp,
         nominal_price float,
         instrument_type instrument_type,
-        currency smallint NOT NULL REFERENCES exchange_db.currency(id)
+        currency smallint NOT NULL REFERENCES currency(id)
 );
 
-CREATE TABLE exchange_db.market (
+CREATE TABLE instrument (
         id int PRIMARY KEY,
-        name varchar,
-        open_time datetime,
-        close_time datetime,
-        delete_date datetime,
-        currency smallint NOT NULL REFERENCES exchange_db.currency(id)
+        delete_date timestamp,
+        lot_size int,
+        trading_start_date timestamp,
+        instrument_template_id smallint NOT NULL REFERENCES  instrument_template(id),
+        market_id smallint NOT NULL REFERENCES market(id)
 );
 
-CREATE TABLE exchange_db.trader (
+CREATE TABLE trader (
       id int PRIMARY KEY,
       first_name varchar,
       last_name varchar,
-      timezone smallint NOT NULL REFERENCES exchange_db.time_zone(id),
-      country smallint NOT NULL REFERENCES exchange_db.country(id),
-      deleted_time datetime
+      timezone smallint NOT NULL REFERENCES time_zone(id),
+      country smallint NOT NULL REFERENCES country(id),
+      deleted_time timestamp
 );
 
-CREATE TABLE exchange_db.broker (
+CREATE TABLE broker (
       id int PRIMARY KEY,
       legal_entity_identifier varchar,
-      timezone smallint NOT NULL REFERENCES exchange_db.time_zone(id),
-      country smallint NOT NULL REFERENCES exchange_db.country(id),
-      commission double,
-      deleted_time datetime,
+      timezone smallint NOT NULL REFERENCES time_zone(id),
+      country smallint NOT NULL REFERENCES country(id),
+      commission numeric(3, 2),
+      deleted_time timestamp,
       actual_address varchar,
       legal_address varchar,
       name varchar
 );
 
-CREATE TABLE exchange_db.account (
-  number varchar PRIMARY KEY,
+CREATE TABLE account (
+  number int PRIMARY KEY,
   current_funds money,
-  trader_code int REFERENCES exchange_db.trader(id),
-  broker_code int NOT NULL REFERENCES exchange_db.broker(id),
+  trader_code int REFERENCES trader(id),
+  broker_code int NOT NULL REFERENCES broker(id),
   type_account type_account,
-  type_currency smallint NOT NULL REFERENCES exchange_db.currency(id),
-  deleted_time datetime
+  type_currency smallint NOT NULL REFERENCES currency(id),
+  deleted_time timestamp
 );
 
-CREATE TABLE exchange_db.depository (
+CREATE TABLE depository (
   id bigint PRIMARY KEY,
   quantity int,
-  instrument_id smallint NOT NULL REFERENCES  exchange_db.instrument(id),
-  account_number int NOT NULL REFERENCES exchange_db.account(number)
+  instrument_id smallint NOT NULL REFERENCES  instrument(id),
+  account_number int NOT NULL REFERENCES account(number)
 );
 
-CREATE TABLE exchange_db.order (
+CREATE TABLE order_ (
   id bigint PRIMARY KEY,
-  place_time datetime,
-  cancel_time datetime,
+  place_time timestamp,
+  cancel_time timestamp,
   price money,
   quantity int,
   status order_status,
   side order_side,
-  account int NOT NULL REFERENCES  exchange_db.account(number),
-  instrument_id int NOT NULL REFERENCES exchange_db.instrument(id),
+  account int NOT NULL REFERENCES  account(number),
+  instrument_id int NOT NULL REFERENCES instrument(id)
 );
 
-CREATE TABLE exchange_db.trade (
+CREATE TABLE trade (
   match_id bigint PRIMARY KEY,
   price money,
   quantity int,
-  buy_order_id bigint NOT NULL REFERENCES exchange_db.order(id),
-  sell_order_id bigint NOT NULL REFERENCES exchange_db.order(id),
-  trade_date datetime
+  buy_order_id bigint NOT NULL REFERENCES order_(id),
+  sell_order_id bigint NOT NULL REFERENCES order_(id),
+  trade_date timestamp
 );
 
-CREATE TABLE exchange_db.movement_fund (
+CREATE TABLE movement_fund (
   id bigint PRIMARY KEY,
   amount money,
   type type_movement_fund,
   initiator_id int,
   initiator_type initiator_type,
-  account_id int NOT NULL REFERENCES exchange_db.account(number)
+  account_id int NOT NULL REFERENCES account(number)
 );
 
 
-CREATE TABLE exchange_db.currency (
-  id smallint,
-  currency_name varchar
-);
 
-CREATE TABLE exchange_db.country (
-  id smallint,
-  country_name varchar
-);
 
-CREATE TABLE exchange_db.time_zone (
-  id smallint,
-  utc_time_zone varchar
-);
+
+
+
 
 
 
