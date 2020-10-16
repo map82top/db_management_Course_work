fdfe7b57ebbd3cc11c822bd44c0e9a3ed0eebd55
isengrims <kapu4inko@gmail.com>
2020-10-16 17:59:37 +0300

    fix instrument template functions

--- instrument_template_functions.sql
+++ instrument_template_functions.sql
@@ -8,7 +8,7 @@ BEGIN
     select id into currency_id from currency where add_instrument_template_human.currency_name = currency.currency_name;
 
     PERFORM add_instrument_template(instrument_code, short_name, long_name, coupon_rate, coupon_amount, coupon_payment_frequency, isin, emission_volume,
-                                    nominal_price, instrument_type, currency_name);
+                                    nominal_price, instrument_type, currency_id);
 END;
 $BODY$
   LANGUAGE plpgsql;
@@ -17,15 +17,15 @@ CREATE OR REPLACE FUNCTION add_instrument_template(instrument_code varchar(6), s
 coupon_amount numeric(2), coupon_payment_frequency smallint, isin varchar(12), emission_volume bigint, nominal_price numeric(2), instrument_type instrument_type,
 currency_id smallint) RETURNS void AS
   $BODY$
-DECLARE
-  currency_id smallint;
 BEGIN
   IF NOT EXISTS (SELECT 1 from currency where currency.id = currency_id) THEN
     RAISE EXCEPTION 'Specified currency does not exists';
   END IF;
 
-  INSERT INTO instrument_template VALUES(instrument_code, short_name, long_name, coupon_rate, coupon_amount, coupon_payment_frequency, isin, emission_volume, 
-  nominal_price, instrument_type, currency_id);
+  INSERT INTO instrument_template(instrument_code, short_name, long_name, coupon_rate, coupon_amount, coupon_payment_frequency, isin, emission_volume, nominal_price, 
+  instrument_type, currency, emission_date) 
+  VALUES(instrument_code, short_name, long_name, coupon_rate, coupon_amount, coupon_payment_frequency, isin, emission_volume, 
+  nominal_price, instrument_type, currency_id, CURRENT_TIMESTAMP);
 
 END;
 $BODY$
@@ -36,7 +36,7 @@ CREATE OR REPLACE FUNCTION remove_instrument_template(instrument_code varchar(6)
     $BODY$
 DECLARE
     market RECORD;
-    instrument RECORD;
+    instrument_ RECORD;
 BEGIN
     IF NOT EXISTS(SELECT 1 from instrument_template where instrument_template.instrument_code = remove_instrument_template.instrument_code) THEN 
         RAISE EXCEPTION 'Instrument does not exist';
@@ -45,15 +45,16 @@ BEGIN
     FOR market in
         SELECT * from market
     LOOP
-        IF(market.close_time < CURRENT_TIME) THEN
+        IF(market.close_time > CURRENT_TIME) THEN
             RAISE EXCEPTION 'At least market % is not closed', market.name;
         END IF;
     END LOOP;
 
-    FOR instrument in 
+    raise notice 'Value: %', instrument_code;
+    FOR instrument_ in 
         SELECT * from instrument where instrument.instrument_template_id = instrument_code
     LOOP
-        PERFORM delete_instrument(instrument.id);
+        PERFORM delete_instrument(instrument_.id);
     END LOOP;
 
     UPDATE instrument_template SET deleted_time = now() where instrument_template.instrument_code = remove_instrument_template.instrument_code;
--- windows/apply.bat
+++ windows/apply.bat
@@ -4,4 +4,5 @@ psql -U postgres -h localhost -d exchange_db -f ../create_shema.sql
 psql -U postgres -h localhost -d exchange_db -f ../broker_functions.sql
 psql -U postgres -h localhost -d exchange_db -f ../trader_functions.sql
 psql -U postgres -h localhost -d exchange_db -f ../account_functions.sql
-psql -U postgres -h localhost -d exchange_db -f test_broker.sql
+psql -U postgres -h localhost -d exchange_db -f ../instrument_template_functions.sql
+psql -U postgres -h localhost -d exchange_db -f test_instrument_template.sql
